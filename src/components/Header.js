307592ad23c1bf139d469e63de4db61a2257552b
Al Hera Ahmad <alheraahmad0786@gmail.com>
2023-03-21 12:49:42 +0530

    feat : added header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import logo from "../assets/images/logo.png";
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="component-header flex-column-center">
+        <div className="img-container">
+        <img src={logo} alt="Component Logo" className="img-responsive"/>
+        </div>
+        <h1 className="center-text">Educase is a one-stop solution to all the additional resources that a school/ preschool need.</h1>
+    </div>
   )
 }
 
