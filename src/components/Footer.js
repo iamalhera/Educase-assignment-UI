cec3e8837557d86188d96a20bed4a5954486db85
Al Hera Ahmad <alheraahmad0786@gmail.com>
2023-03-21 12:51:30 +0530

    feat : added footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { LogosGoogleIcon, LogosFacebook, LogosTwitter } from '../assets/icons/icons'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="component-footer flex-column-center">
+      <p>or login with</p>
+      <div className="social-icons-panel">
+        <div className="social-icon">
+          <LogosGoogleIcon />
+        </div>
+        <div className="social-icon">
+          <LogosFacebook />
+        </div>
+        <div className="social-icon">
+          <LogosTwitter />
+        </div>
+      </div>
+      <p>Don't have an Account? <span className="highlighted-text"> Create new now!</span></p>
+      <p>By signing up, you agree with out  <span className="highlighted-text"> Terms & Conditions</span></p>
+    </div>
   )
 }
 
