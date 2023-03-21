33bf1ffd4d40433fc6cf3472550350ed0835796a
Al Hera Ahmad <alheraahmad0786@gmail.com>
2023-03-21 10:16:33 +0530

    initial commit : installed react, added folder structure

---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Header'
+
+const Login = () => {
+  return (
+    <div className="component-form">
+        <Header />
+    </div>
+  )
+}
+
+export default Login
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Footer from "./Footer";
+import Form from "./Form";
+import Header from "./Header";
+import Login from "./Login";
+import Signin from "./Signin";
+import Signout from "./Signout";
+
+export {Footer, Form, Header, Login, Signin, Signout }
