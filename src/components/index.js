ee952aebfd167c76c0550ceca1688e576cb906cc
Al Hera Ahmad <alheraahmad0786@gmail.com>
2023-03-21 12:50:51 +0530

    Delete index.js

--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-import Footer from "./Footer";
-import Form from "./Form";
-import Header from "./Header";
-import Login from "./Login";
-import Signin from "./Signin";
-import Signout from "./Signout";
-
-export {Footer, Form, Header, Login, Signin, Signout }
