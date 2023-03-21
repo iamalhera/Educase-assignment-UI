25b51c5a4dda4cf2a44f4c4a7585ba8eb5ca68d1
Al Hera Ahmad <alheraahmad0786@gmail.com>
2023-03-21 12:50:06 +0530

    feat : added form component and tab functionality

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Signout from './Signout';
+import Signin from './Signin';
 
 const Form = () => {
+  const [login, setLogin] = useState(true);
   return (
-    <div>Form</div>
+    <div className="component-form">
+      <div className="form-tab flex-row">
+        <div
+          className={`tab-btn center-text ${login === true ? "btn-active" : ""}`}
+          onClick={() => setLogin(!login)}
+        >
+          <p>Login</p> </div>
+        <div
+          className={`tab-btn center-text ${login === false ? "btn-active" : ""}`}
+          onClick={() => setLogin(!login)}
+        >
+          <p> Sign up</p> </div>
+      </div>
+      {
+        !login && <Signout />
+      }
+      {
+        login && <Signin />
+      }
+    </div>
   )
 }
 
